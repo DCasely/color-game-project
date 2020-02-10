5925360673d4d842914b4342110c24af00583588
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-02-10 12:52:31 -0500

    Creating the Color Game pt 6

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector('h1');
 var resetButton = document.querySelector('#reset');
+var easyBtn = document.querySelector('#easyBtn');
+var hardBtn = document.querySelector('#hardBtn');
+
+// EASY GAME MODE
+easyBtn.addEventListener('click', function() {
+  hardBtn.classList.remove('selected');
+  easyBtn.classList.add('selected');
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = 'none';
+    }
+  }
+});
+
+// HARD GAME MODE
+hardBtn.addEventListener('click', function() {
+  hardBtn.classList.add('selected');
+  easyBtn.classList.remove('selected');
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = 'block';
+  }
+});
 
 resetButton.addEventListener('click', function() {
   // generate all new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   // pick a new random color from array
   pickedColor = pickColor();
   // change colorDisplay to match picked Color
@@ -17,7 +51,7 @@ resetButton.addEventListener('click', function() {
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.background = colors[i];
   }
-  h1.style.background = '#232323';
+  h1.style.backgroundColor = '#232323';
 });
 
 colorDisplay.textContent = pickedColor;
